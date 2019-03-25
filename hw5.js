e37074abe666ef4fd68ec5d95a55546ebe4f3d9f
norahx3x <alnoor0416@gmail.com>
2019-03-25 09:16:04 +0300

    start point

---
+++ hw5.js
@@ -0,0 +1,15 @@
+
+var allcites = []
+document.getElementById('selCity').addEventListener('change',getCityInfo,false)
+//document.getElementById('').addEventListener('click',,false)
+function getCityInfo(id) {
+  
+}
+
+
+listAll()
+
+// list all cities 
+function listAll() {
+   
+}
