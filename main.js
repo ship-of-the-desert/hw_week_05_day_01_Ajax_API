9b188d63ce29b2336e11c164c2fc5b321a00bb04
alhanoufoth33 <alhanoufoth33@gmail.com>
2019-03-25 09:32:59 +0300

    updated

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
 //$(document).ready(function(){
 //$('#submitWeather').click(function(){
-    $("#submitWeather").click(function(){
-        let input = $('#input').val()
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=fb63f61bac384b0429d3ceea3379f796`
+ 
         
+       
+function weather() {
+    var cityName = $('#cityName').val();
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=fb63f61bac384b0429d3ceea3379f796';
+
+     $.getJSON(url, weatherDetails);
+
+     function weatherDetails (data) {
+        var cityName = data.name;
+        var temp = (data.main.temp).toFixed(1);
+        var condition = data.weather[0].main;
+        var wind = data.wind.speed;
+        // console.log(data);
+
+         $('.show').append("The weather in  " + cityName + "  is: <br>" + 
+         " Temprature: " + temp + "<br>Condition: " + condition + 
+         "<br> Wind Speed: " + wind + "<br><br>");
+
+     }
+} 
         
-        $.get(url, function(data){
-            let city = data.name
-            let country = data.sys.country
-            let weatherDesc = data.weather[0].main
-            let temp = (data.main.temp - 273.15).toFixed(1)
-
-
-
-         $('#city').text(city);
-         $('#country').text(country)
-         $('#weatherDesc').text(weatherDesc)
-         $('#temp').text(temp)
-
-
-
-//    var url = 'api.openweathermap.org/data/2.5/weather?q=' + city +
-//     '&APPID=fb63f61bac384b0429d3ceea3379f796';
-// }
-
-    
-// $.getJSON(url, weatheDetails);
-
-// function weatheDetails(data){
-//     var city = data.name;
-//     var temp = data.main.temp;
-//     var condition = data.weather[0].main;
